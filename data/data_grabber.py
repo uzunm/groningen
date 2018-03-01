e3a4c795f338d26ca47487c77bc6726ea8a6946a
james <jjlong@mit.edu>
2018-03-01 05:01:00 -0500

    created skeleton for fetching earthquake data based on events catalog, and chosen set of stations and channels;

--- data/data_grabber.py
+++ data/data_grabber.py
@@ -4,30 +4,77 @@ import io
 import matplotlib
 import pandas as pd
 import helpers
-def get_event_response(starttime, endtime, network, station, channel):
+import functools
+import datetime
+import copy
+
+def get_event_response(network=None, station=None, channel=None,
+					   eventid=None,starttime=None, endtime=None):
     """get an event mseed file from the knmi database given
     query parameters describing the event"""
     baseurl = 'http://rdsa.knmi.nl/fdsnws/dataselect/1/query'
     payload = {'starttime':starttime,'endtime':endtime,'network':network,
                'station':station, 'channel':channel,'nodata':404}
     r = requests.get(baseurl,params=payload)
-    print(r.status_code)
-    return {'data':r.content,'starttime':starttime,'endtime':endtime
-            ,'channel':channel,'station':station}
+    if r.status_code ==200:
+    	return {'data':r.content,'starttime':starttime,'endtime':endtime
+                ,'channel':channel,'station':station, '_id':eventid}
+@helpers.bind
 def streamify(event_response):
     data = event_response['data']
     st = obspy.read(io.BytesIO(data))
-    useful_keys = ['starttime','endtime','channel','station']
+    useful_keys = ['starttime','endtime','channel','station','_id']
     streamified = {'stream':st}
     for k in useful_keys:
         streamified[k]=event_response[k]
     return streamified
+@helpers.bind
+def stream_formatter(streamified):
+	"""murat fill in here"""
+	return streamified
+def add_to_timestring(nseconds, isostring):
+	"""given a timestamp in iso8601 string format, add a timedelta
+	of nseconds to the time and return
+	a new iso8601 string with the incremented time."""
+	currenttime = datetime.datetime.strptime(isostring,"%Y-%m-%dT%H:%M:%S.%f")
+	delta = datetime.timedelta(seconds=nseconds)
+	futuretime = currenttime+delta
+	return datetime.datetime.strftime(futuretime,"%Y-%m-%dT%H:%M:%S.%f")
+def parse_events(f='events.psv', incrementer = functools.partial(add_to_timestring,300)):
+	df = pd.read_csv(f, delimiter = '|',encoding='utf-8')[['#EventID','Time']]
+	return ({'event':{'eventid':eventid,'starttime':time, 'endtime':incrementer(time)}} for index,eventid,time in df.itertuples())
+
 get_and_streamify = helpers.pipe(get_event_response, streamify) #pipe(f,g)() is equivalent to f(g())
+get_and_format = helpers.pipe(get_event_response, streamify, stream_formatter)
+def unroll(job_d):
+	event_d =job_d['event']
+	del job_d['event']
+	return helpers.merge_dicts(job_d, event_d)
+def create_jobs_queue():
+	"""create a generator of job parameter dicts like:
+	[{'starttime':s,'endtime':e,'network':n,
+               'station':s, 'channel':c},,,"""
+	n = {'network': ['NL']}
+	stations = {'station':['VKB']}
+	channels = {'channel':['BHZ']}
+	events = helpers.lstdcts2dctlsts(helpers.first_n(parse_events(), 5))
+	all_combs = helpers.many_dict_product(n, stations, channels, events)
+	return map(unroll,all_combs)
+
+def serial_worker(jobs_queue):
+	"""given an iterator of event dictionaries 
+	{'starttime':s,'endtime':e,'network':n,
+     'station':s, 'channel':c}
+	get the http response for that set of parameteres,,
+	extract the mseed stream and format it."""
+	return (get_and_format(**job) for job in jobs_queue)
+def parallel_worker(jobs_queue):
+	"""multithreaded concurrent version of serial worker"""
+	jobs = (functools.partial(get_and_format, **job) for job in jobs_queue)
+	res = helpers.run_chunks_parallel(jobs, chunksize = 10)
+	return res
 if __name__ == '__main__':
-	tstart='2018-02-18T14:05:10.2'
-	tend = '2018-02-18T14:05:15.2'
-	network = 'NL'
-	station= 'VKB'
-	channel = 'BHZ'
-	streamified = get_and_streamify(tstart, tend, network, station, channel)
-	print(streamified)
+	q = create_jobs_queue()
+	res = parallel_worker(q)
+	for r in res:
+		print(r)
--- data/helpers.py
+++ data/helpers.py
@@ -1,4 +1,15 @@
 import functools
+import itertools
+import collections
+import asyncio
+import concurrent
+def bind(f):
+    def wrapper(x):
+        if x is None:
+            return None
+        else:
+            return f(x)
+    return wrapper
 
 def compose_two(func1, func2):
      def composition(*args, **kwargs):
@@ -8,3 +19,47 @@ def compose(*funcs):
     return functools.reduce(compose_two, funcs)
 def pipe(*funcs):
     return functools.reduce(compose_two, reversed(funcs))
+def dict_product(d):
+    """e.g.d = {'A':[0,1]
+                ,'B':[3]}
+    return [{'A':0, 'B':3}
+            {'A':1,'B':3}]"""
+    ks = d.keys()#e.g. ['A,'B']
+    values = d.values() #e.g. [[0,1],[3]]
+    products = itertools.product(*values) #e.g. ...[(0,3),(1,3)]
+    zipped = (zip(ks, product) for product in products) #e.g.[ [('A",0),('B',3)],[('A",1),('B',3)]]
+    return (dict(tpl) for tpl in zipped)
+def merge_dicts(*dcts):
+    return dict(collections.ChainMap({}, *dcts))
+many_dict_product = pipe(merge_dicts, dict_product)
+def lstdcts2dctlsts(lst):
+    """e.g. [{'A':2},{'A':3}] -> {'A':[2,3]}"""
+    def accumer(accum, el):
+        for k,v in el.items():
+            accum[k].append(v)
+        return accum
+    return dict(functools.reduce(accumer, lst, collections.defaultdict(list)))
+def first_n(seq, n):
+    return (x for _,x in zip(range(n), seq))
+def grouper(n, iterable):
+    """
+    >>> list(grouper(3, 'ABCDEFG'))
+    [['A', 'B', 'C'], ['D', 'E', 'F'], ['G']]
+    """
+    iterable = iter(iterable)
+    return iter(lambda: list(itertools.islice(iterable, n)), [])
+async def parallelmain(curriedjobs, workers=20):
+    with concurrent.futures.ThreadPoolExecutor(max_workers=workers) as executor:
+        loop = asyncio.get_event_loop()
+        futures = [loop.run_in_executor(executor, f) for f in curriedjobs]
+        all_res = [response for response in await asyncio.gather(*futures)]
+        return all_res
+def run_parallel(curriedjobs, workers=20):
+    loop = asyncio.get_event_loop()
+    all_res = loop.run_until_complete(parallelmain(curriedjobs, workers))
+    return all_res
+def run_chunks_parallel(jobs,chunksize = 100, workers = 20):
+    chunked = grouper(chunksize,jobs)
+    res_chunks = (run_parallel(chunk, workers=workers) for chunk in chunked)
+    flat_res = itertools.chain(*res_chunks)
+    return flat_res
